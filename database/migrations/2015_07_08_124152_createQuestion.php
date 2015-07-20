44fcb04d00c456e627ad61cf19e1b773a8687bc7
aydogm_i <ismaydogmus@gmail.com>
2015-07-20 19:57:14 +0200

    the create ask database

---
+++ database/migrations/2015_07_08_124152_createQuestion.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ask', 255)->unique();
+            $table->text('idAnswer');
+            $table->integer('idMaster');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
