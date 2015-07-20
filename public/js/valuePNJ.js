186c431ee78115fc7fb4d1f7dc6e3bf5c1749006
aydogm_i <ismaydogmus@gmail.com>
2015-07-20 18:12:35 +0200

    when add some value for the pnj, the script say the value

---
+++ public/js/valuePNJ.js
@@ -0,0 +1,35 @@
+/*jslint browser: true, node : true*/
+/*jslint devel : true*/
+/*global $, next*/
+function HPValue() {
+    "use strict";
+    document.getElementById("valeurHP").innerHTML = document.getElementById("HP").value;
+}
+function ADValue() {
+    "use strict";
+    document.getElementById("valeurAD").innerHTML = document.getElementById("AD").value;
+}
+function APValue() {
+    "use strict";
+    document.getElementById("valeurAP").innerHTML = document.getElementById("AP").value;
+}
+function armorValue() {
+    "use strict";
+    document.getElementById("valeurArmor").innerHTML = document.getElementById("armor").value;
+}
+function MRValue() {
+    "use strict";
+    document.getElementById("valeurMR").innerHTML = document.getElementById("MR").value;
+}
+function peneArmorValue() {
+    "use strict";
+    document.getElementById("valeurPeneArmor").innerHTML = document.getElementById("peneArmor").value;
+}
+function peneMRValue() {
+    "use strict";
+    document.getElementById("valeurPeneMR").innerHTML = document.getElementById("peneMR").value;
+}
+function prixValue() {
+    "use strict";
+    document.getElementById("valeurPrix").innerHTML = document.getElementById("prix").value;
+}
